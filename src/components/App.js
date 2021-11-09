e37571905403cebc93bb6e3984c61751fff373d0
carlosalfarodev <rgkalin@gmail.com>
2021-11-09 05:18:50 -0800

    add API call to get pokemons

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import '../css/style.css';
 
+const pokeURL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
+
+async function fetchPokemons() {
+  const response = await fetch(pokeURL, {
+    headers: { accept: 'application/json' },
+  });
+  const pokemons = await response.json();
+  console.log(pokemons);
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Poke App</h1>
+      <button onClick={fetchPokemons} className="btn btn-get-pokemons">
+        Show me Pokemons
+      </button>
     </div>
   );
 }
