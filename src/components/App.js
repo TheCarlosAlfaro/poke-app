14d1ae31ef72bb583b4835671f43196cbb7b45a1
carlosalfarodev <rgkalin@gmail.com>
2021-11-09 06:03:35 -0800

    adds loader with conditional rendering using useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,37 @@
+import { useState } from 'react';
 import '../css/style.css';
 
-const pokeURL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
-
-async function fetchPokemons() {
-  const response = await fetch(pokeURL, {
-    headers: { accept: 'application/json' },
-  });
-  const pokemons = await response.json();
-  console.log(pokemons);
-}
-
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const pokeURL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
+
+  async function fetchPokemons() {
+    setIsLoading(true);
+    const response = await fetch(pokeURL, {
+      headers: { accept: 'application/json' },
+    });
+    const data = await response.json();
+    const pokemons = data.results;
+    setPokemons(pokemons);
+    setIsLoading(false);
+    console.log(pokemons);
+  }
+
   return (
     <div className="App">
       <h1>Welcome to Poke App</h1>
       <button onClick={fetchPokemons} className="btn btn-get-pokemons">
         Show me Pokemons
       </button>
+      {isLoading && (
+        <div className="lds-ripple loader">
+          <div></div>
+          <div></div>
+        </div>
+      )}
+      {pokemons.length > 0 && <h1>It works</h1>}
     </div>
   );
 }
